c37cce4b3ff51c299060a3a6f88ed65ad94c5f97
outsung <insung9546@gmail.com>
2021-01-04 22:26:18 +0900

    Slerp increase

--- src/components/Slerp.tsx
+++ src/components/Slerp.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo, useRef } from 'react';
-import * as THREE from 'three';
-import { useFrame, useThree } from 'react-three-fiber';
+import React, { useMemo, useRef } from "react";
+import * as THREE from "three";
+import { useFrame, useThree } from "react-three-fiber";
 
 type slerpProps = {
   children: React.ReactNode;
@@ -12,14 +12,14 @@ function Slerp({ children, ...args }: slerpProps) {
 
   const [rotationEuler, rotationQuaternion] = useMemo(
     () => [new THREE.Euler(0, 0, 0), new THREE.Quaternion(0, 0, 0, 0)],
-    [],
+    []
   );
 
   useFrame(({ mouse }) => {
     if (!group.current) return;
 
-    const x = (mouse.x * viewport.width) / 100;
-    const y = (mouse.y * viewport.height) / 100;
+    const x = (mouse.x * viewport.width) / 20;
+    const y = (mouse.y * viewport.height) / 20;
     rotationEuler.set(y, x, 0);
     rotationQuaternion.setFromEuler(rotationEuler);
     group.current.quaternion.slerp(rotationQuaternion, 0.1);
